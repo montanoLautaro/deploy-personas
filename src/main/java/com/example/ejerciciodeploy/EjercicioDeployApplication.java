0e81bf26e0e958143f51b40b19a5a50b707c28bc
Montaño Heredia Lautaro Nahuel <heredialautaro@gmail.com>
2023-01-17 19:50:19 -0300

    a

--- src/main/java/com/example/ejerciciodeploy/EjercicioDeployApplication.java
+++ src/main/java/com/example/ejerciciodeploy/EjercicioDeployApplication.java
@@ -1,13 +1,20 @@
 package com.example.ejerciciodeploy;
 
+import com.example.ejerciciodeploy.models.Persona;
+import com.example.ejerciciodeploy.repositories.PersonaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class EjercicioDeployApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EjercicioDeployApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EjercicioDeployApplication.class, args);
+		PersonaRepository personaRepository = context.getBean(PersonaRepository.class);
+
+		personaRepository.save(new Persona(null, "Romina", "Gomez", "Estudiante"));
+
 	}
 
 }
